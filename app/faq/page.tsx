507e53555c85630c6746595c655985c9d54db971
scotex1 <satyampal8303@gmail.com>
2026-07-21 19:27:43 +0300

    Add files via upload

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,1 +1,15 @@
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import { FAQ } from "@/components/landing/FAQ";
 
+export default function FAQPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <FAQ />
+      </main>
+      <Footer />
+    </>
+  );
+}
